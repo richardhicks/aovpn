303486b267f63421ef0bd69b19da2a7865bc8856
Richard M. Hicks <rich@richardhicks.com>
2019-01-05 13:51:29 -0800

    Add files via upload
    Added New-AovpnConnection.ps1 for creating Windows 10 Always On VPN user tunnels and New-AovpnDeviceConnection.ps1 for creating Always On VPN device tunnels.

---
+++ New-AovpnDeviceConnection.ps1
@@ -0,0 +1,45 @@
+[CmdletBinding()]
+
+Param(
+
+[string]$xmlFilePath,
+[string]$ProfileName
+
+)
+
+$ProfileXML = Get-Content $xmlFilePath
+
+$ProfileNameEscaped = $ProfileName -replace ' ', '%20'
+
+$ProfileXML = $ProfileXML -replace '<', '&lt;'
+$ProfileXML = $ProfileXML -replace '>', '&gt;'
+$ProfileXML = $ProfileXML -replace '"', '&quot;'
+
+$nodeCSPURI = './Vendor/MSFT/VPNv2'
+$namespaceName = "root\cimv2\mdm\dmmap"
+$className = "MDM_VPNv2_01"
+
+$session = New-CimSession
+
+try
+{
+$newInstance = New-Object Microsoft.Management.Infrastructure.CimInstance $className, $namespaceName
+$property = [Microsoft.Management.Infrastructure.CimProperty]::Create("ParentID", "$nodeCSPURI", 'String', 'Key')
+$newInstance.CimInstanceProperties.Add($property)
+$property = [Microsoft.Management.Infrastructure.CimProperty]::Create("InstanceID", "$ProfileNameEscaped", 'String', 'Key')
+$newInstance.CimInstanceProperties.Add($property)
+$property = [Microsoft.Management.Infrastructure.CimProperty]::Create("ProfileXML", "$ProfileXML", 'String', 'Property')
+$newInstance.CimInstanceProperties.Add($property)
+
+$session.CreateInstance($namespaceName, $newInstance)
+$Message = "Created $ProfileName profile."
+Write-Host "$Message"
+}
+catch [Exception]
+{
+$Message = "Unable to create $ProfileName profile: $_"
+Write-Host "$Message"
+exit
+}
+$Message = "Complete."
+Write-Host "$Message"
