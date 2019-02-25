2dac4f9c0f7bf019105fd7c3cc66c090d8c02066
Richard M. Hicks <rich@richardhicks.com>
2019-02-25 10:57:12 -0800

    Update Set-IKEv2VpnSecurityBaseline.ps1
    Add PowerShell command to revert to default settings prior to implementing new settings.

--- Set-IKEv2VpnSecurityBaseline.ps1
+++ Set-IKEv2VpnSecurityBaseline.ps1
@@ -1,4 +1,5 @@
 # // Configure IKEv2 VPN baseline security on Windows Server Routing and Remote Access (RRAS) servers
 
+Set-VpnServerConfiguration -RevertToDefault | Out-Null
 Set-VpnServerConfiguration -CustomPolicy -AuthenticationTransformConstants SHA256128 -CipherTransformConstants AES128 -DHGroup Group14 -EncryptionMethod AES128 -IntegrityCheckMethod SHA256 -PFSgroup PFS2048 -SADataSizeForRenegotiationKilobytes 102400
 Restart-Service RemoteAccess -PassThru
