35a68f5006cd7d2dc47e72c71f88e54877dbd5ca
Richard M. Hicks <rich@richardhicks.com>
2019-05-24 16:20:41 -0700

    Update Enable-VpnServerIKEv2Fragmentation.ps1

--- Enable-VpnServerIKEv2Fragmentation.ps1
+++ Enable-VpnServerIKEv2Fragmentation.ps1
@@ -1,3 +1,4 @@
-# // enable IKEv2 server fragmentation for Windows Server RRAS
+# // Enable IKEv2 fragmentation suport for Windows Routing and Remote Access Service (RRAS)
+# // Requires Windows Server 2019 (does not work with Windows Server 2016 or Windows Server 2012/R2)
 
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters\Ikev2\" -Name EnableServerFragmentation -PropertyType DWORD -Value 1 -Force
