0ba66d4c43c5140b65a715d5ca3a716a299e1a97
Richard M. Hicks <rich@richardhicks.com>
2020-08-23 22:02:35 -0700

    Create New-AovpnDeviceConnection.ps1

---
+++ New-AovpnDeviceConnection.ps1
@@ -0,0 +1,3 @@
+The PsowerShell script New-AovpnDeviceConnection has been deprecated. Please use New-AovpnConnection.ps1 with the -DeviceTunnel switch instead.
+
+<a href="https://github.com/richardhicks/aovpn/blob/master/New-AovpnConnection.ps1">https://github.com/richardhicks/aovpn/blob/master/New-AovpnConnection.ps1</a>
