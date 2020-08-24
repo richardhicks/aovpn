b9872bc51de311d86c9c7e3e6d822979cf87c5f3
Richard M. Hicks <rich@richardhicks.com>
2020-08-23 22:04:29 -0700

    Add files via upload

---
+++ New-AovpnDeviceConnection.ps1
@@ -0,0 +1,3 @@
+The PowerShell script New-AovpnDeviceConnection.ps1 has been deprecated. Please use New-AovpnConnection.ps1 with the -DeviceTunnel switch instead.
+
+https://github.com/richardhicks/aovpn/blob/master/New-AovpnConnection.ps1
