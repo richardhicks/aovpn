4eeb291dca8dbf820596326a28b4c0d0d780a451
Richard M. Hicks <rich@richardhicks.com>
2020-08-23 22:03:49 -0700

    Delete New-AovpnDeviceConnection.ps1

--- New-AovpnDeviceConnection.ps1
+++
@@ -1,3 +0,0 @@
-The PsowerShell script New-AovpnDeviceConnection has been deprecated. Please use New-AovpnConnection.ps1 with the -DeviceTunnel switch instead.
-
-https://github.com/richardhicks/aovpn/blob/master/New-AovpnConnection.ps1
