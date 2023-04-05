19b559d552518a64d044eca36f0859e04dc9962a
Richard M. Hicks <rich@richardhicks.com>
2023-04-05 09:58:14 -0700

    Create Optimize-InboxAccountingDatabaseManagement.ps1

---
+++ Optimize-InboxAccountingDatabaseManagement.ps1
@@ -0,0 +1,7 @@
+This file has been moved to the InboxAccountingDatabaseManagement PowerShell module.
+
+https://github.com/richardhicks/InboxAccountingDatabaseManagement
+
+https://www.powershellgallery.com/packages/InboxAccountingDatabaseManagement/
+
+Install-Module InboxAccountingDatabaseManagement
